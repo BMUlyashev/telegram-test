c64407754d6fd61a48322ed3c8e20e0d57c09e8c
b.m.ulyashev <b.m.ulyashev@mail.ru>
2022-12-18 14:39:09 +0300

    init.

---
+++ src/test/java/com/example/telegramtest/TelegramTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.telegramtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
